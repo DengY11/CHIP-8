8c75dd1dfdb8be2e11d1145cdc2855042cf7ddbd
dengyi.0818 <dengyi.0818@dcarlife.com>
2025-09-29 16:18:39 +0800

    feat: impl display module

--- src/stack.hpp
+++ src/stack.hpp
@@ -2,19 +2,20 @@
 #include <cstdint>
 #include <optional>
 
-namespace CHIP8{
+namespace CHIP8 {
 
 static constexpr uint8_t STACK_SIZE = 16;
 
-class Stack{
+class Stack {
 public:
     Stack();
     ~Stack() = default;
     bool push(uint16_t value);
     std::optional<uint16_t> pop();
+
 private:
     uint16_t stack[STACK_SIZE];
     uint8_t sp;
 };
 
-}
+}  // namespace CHIP8
